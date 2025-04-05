5ba7b3231bb0775e7ea08aeb737523fd804113ba
landmuc <landmucdev@gmail.com>
2025-04-05 20:32:15 +0200

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.4"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("org.graalvm.buildtools.native") version "0.10.6"
+}
+
+group = "com.landmuc"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springShellVersion"] = "3.4.0"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.shell:spring-shell-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.springframework.shell:spring-shell-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.shell:spring-shell-dependencies:${property("springShellVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/com/landmuc/spotcli/SpotcliApplication.java
@@ -0,0 +1,13 @@
+package com.landmuc.spotcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotcliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotcliApplication.class, args);
+	}
+
+}
